40c9a5cedaf207a1ce7a91fb28f79850a1a46580
Natnael Mehari <natnaelmehari27@gmail.com>
2025-03-02 12:37:50 +0100

    learned stateful components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,15 @@
 import css from './App.module.css';
 import SideBar from './components/Sidebar';
+import StatefulGreeting from './components/StatefulGreeting';
+import NavBarSimple from './components/NavBarSimple';
 
 function App() {
   return (
     <div className={css.App}>
+      <NavBarSimple />
+      <StatefulGreeting /> 
       <SideBar />
+      
     </div>
   );
 }
---
+++ my-app/src/components/NavBarSimple.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import css from "./css/NavBarSimple.module.css";
+
+class NavBarSimple extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            introduction: "Hello, guest!",
+            buttonText: "log in",
+        }
+    }
+
+    handleclick = () => {
+        this.setState((prevState) => ({
+            message: prevState.message === "Hello, guest!" ? "Welcome back, user!" : "Hello, guest!",
+            buttonText: prevState.buttonText === "Log in" ? "Log out" : "Log in",
+        }), ()=> console.log(this.state.message))
+        
+    }
+
+    render() {
+        return(
+            <div className={css.NavBar}>
+                <h1>My Gallery</h1>
+                <div>
+                    <span>{this.state.message}</span>
+                    <button onClick={() => this.handleclick()}>{this.state.buttonText}</button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default NavBarSimple
